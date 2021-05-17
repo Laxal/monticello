f64c926a9eaeb03b4f9d5cf0782ffc89813b8f3e
Olexandr Nychyporuk <lax.nich33@gmail.com>
2021-05-17 03:28:05 +0300

    section1 completed

--- js/section1-slider.js
+++ js/section1-slider.js
@@ -1,5 +1,14 @@
 $(document).ready(function () {
     $('.section1-slider').slick({
-
+        dots: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 4000,
+        speed: 1500,
+        pauseOnHover: false,
+        pauseOnDotsHover: true,
     });
 });
