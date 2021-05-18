432daf241f598cabf4a3e8ec228aa6688d0935ae
Olexandr Nychyporuk <lax.nich33@gmail.com>
2021-05-18 13:41:06 +0300

    section3 comleted

--- js/sliders.js
+++ js/sliders.js
@@ -11,4 +11,17 @@ $(document).ready(function () {
         pauseOnHover: false,
         pauseOnDotsHover: true,
     });
+
+    $('.section3__slider').slick({
+        dots: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        arrows: true,
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 4000,
+        speed: 800,
+        pauseOnHover: false,
+        pauseOnDotsHover: true,
+    })
 });
